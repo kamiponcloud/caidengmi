9fddb6e70ae195d6eb499240467dcf88aaccd80d
kamiponcloud <1906219821@qq.com>
2020-05-21 13:58:38 +0800

    select

--- JavaScript.js
+++ JavaScript.js
@@ -1,5 +1,10 @@
 function myFunction() {
     x=document.getElementById("head");
     x.innerHTML = "没心情";
-    
+    x=document.getElementById("A");
+    x.innerHTML = "青";
+    x=document.getElementById("B");
+    x.innerHTML = "清";
+    x=document.getElementById("C");
+    x.innerHTML = "请";
 }
